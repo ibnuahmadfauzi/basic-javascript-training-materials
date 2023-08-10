b1eb1355dfa9d2b0c6ad48ba5c5c5581bb33b239
ibnuahmadfauzi <ibnu7897@gmail.com>
2023-08-10 20:16:29 +0700

    Membuat latihan function

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-// Menangkap pilihan player
-var player = prompt("pilih : gajah / semut / orang");
+var volumeKubus1 = prompt("Volume Kubus 1");
+var volumeKubus2 = prompt("Volume Kubus 2");
 
-// Menangkap pilihan computer
-// Membangkitkan bilangan random
-var bot = Math.random();
-if ( bot >= 0 && bot <= 0.3 ) {
-	bot = "gajah";
-} else if ( bot > 0.3 && bot <= 0.6 ) {
-	bot = "semut";
-} else {
-	bot = "orang";
+function jumlahVolumeKubus(a, b) {
+	var v = a**3 + b**3;
+	return v;
 }
 
-// Menentukan rules
-var hasil = "hasil";
-if ( bot == player ) {
-	hasil = "seri";
-} else if ( (bot == "gajah" && player == "orang") || (bot == "orang" && player == "semut") || (bot == "semut" && player == "gajah") ) {
-	hasil = "kalah";
-} else if ( (bot == "gajah" && player == "semut") || (bot == "orang" && player == "gajah") || (bot == "semut" && player == "orang") ) {
-	hasil = "menang";
-} else {
-	hasil = "tidak ada keputusan";
-}
-
-// Tampilkan hasil
-console.log("Bot    : " + bot);
-console.log("Player : " + player);
-console.log("Hasil  : " + hasil);
+alert("Jumlah Volume Kubus : " + jumlahVolumeKubus(volumeKubus1, volumeKubus2));
