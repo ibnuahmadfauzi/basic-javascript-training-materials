8953a17264d10c56e090c8de124c0cc95bc2537f
ibnuahmadfauzi <ibnu7897@gmail.com>
2023-08-12 20:12:21 +0700

    Membuat pengelolaan penumpang angkot menggunakan array

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-var volumeKubus1 = prompt("Volume Kubus 1");
-var volumeKubus2 = prompt("Volume Kubus 2");
+var penumpang = [];
 
-function jumlahVolumeKubus(a, b) {
-	var v = a**3 + b**3;
-	return v;
+var tambahPenumpang = function(namaPenumpang, penumpang) {
+
+    if ( penumpang.length == 0 ) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+    } else {
+        for( var i = 0; i < penumpang.length; i++ ) {
+            if( penumpang[i] == undefined ) {
+                penumpang[i] = namaPenumpang;
+                return penumpang;
+            } else if ( penumpang[i] == namaPenumpang ) {
+                console.log(namaPenumpang + " sudah ada di dalam angkot");
+                return penumpang;
+            } else if ( i == penumpang.length - 1 ) {
+                penumpang.push(namaPenumpang);
+                return penumpang;
+            }
+        }
+    }
+
 }
 
-alert("Jumlah Volume Kubus : " + jumlahVolumeKubus(volumeKubus1, volumeKubus2));
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    if ( penumpang.length == 0 ) {
+        console.log("Angkot masih kosong.");
+        return penumpang;
+    } else {
+        for ( var i = 0; i < penumpang.length; i++ ) {
+            if ( penumpang[i] == namaPenumpang ) {
+                penumpang[i] = undefined;
+                return penumpang;
+            } else if ( i == penumpang.length - 1 ) {
+                console.log(namaPenumpang + ' tidak ada di dalam angkot.');
+                return penumpang;
+            }
+        }
+    }
+}
